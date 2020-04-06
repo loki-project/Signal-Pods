5b6fd3a2332046d3b9412bb2e4483bab50b2e084
gmbnt <andriesseniels@gmail.com>
2020-04-06 15:28:44 +1000

    Update pods

--- SignalMetadataKit/Loki/SessionResetProtocol.swift
+++
@@ -1,7 +0,0 @@
-
-@objc(LKSessionResetProtocol)
-public protocol SessionResetProtocol {
-    
-    func validatePreKeyForFriendRequestAcceptance(for recipientID: String, whisperMessage: CipherMessage, protocolContext: Any?) throws
-    func getSessionResetStatus(for recipientID: String, protocolContext: Any?) -> SessionResetStatus
-}
